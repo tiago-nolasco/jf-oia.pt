1c9fc8aca1acf48f4e8fa0cb1f039d85b04cfcae
Tiago Nolasco <50207626+tiago-nolasco@users.noreply.github.com>
2023-04-19 22:53:21 +0100

    Responsive

--- src/styles/components/components.ts
+++ src/styles/components/components.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Breakpoints } from "../settings/breakpoints";
 
 export const Main = styled.div``;
 
@@ -6,14 +7,22 @@ export const Aside = styled.div``;
 
 export const ViewContainer = styled.div`
   display: flex;
-  
+
   & > ${Main} {
     width: 75%;
     padding-right: 20px;
+
+    @media only screen and (max-width: ${Breakpoints.LG}) {
+      width: 100%;
+    }
   }
   & > ${Aside} {
     width: 25%;
     padding-left: 20px;
+
+    @media only screen and (max-width: ${Breakpoints.LG}) {
+      display: none;
+    }
   }
 `;
 
@@ -22,8 +31,8 @@ interface ISpacerProps {
   height?: number;
 }
 export const Spacer = styled.div<Partial<ISpacerProps>>`
-  width: ${props => props.width ? `${props.width}px` : "100%"};
-  height: ${props => props.height || 20}px;
+  width: ${(props) => (props.width ? `${props.width}px` : "100%")};
+  height: ${(props) => props.height || 20}px;
 `;
 
 interface IImageProps {
@@ -33,10 +42,10 @@ interface IImageProps {
   height?: number;
 }
 export const Image = styled.div<Partial<IImageProps>>`
-  width: ${props => props.width || "100%"};
-  height: ${props => props.height || "250"}px;
-  background: url(${props => props.src}) no-repeat center center;
-  background-size: ${props => props.fullImage ? "contain" : "cover"};
+  width: ${(props) => props.width || "100%"};
+  height: ${(props) => props.height || "250"}px;
+  background: url(${(props) => props.src}) no-repeat center center;
+  background-size: ${(props) => (props.fullImage ? "contain" : "cover")};
 `;
 
 interface IFlexProps {
@@ -44,5 +53,5 @@ interface IFlexProps {
 }
 export const Flex = styled.div<Partial<IFlexProps>>`
   display: flex;
-  flex-flow: ${props => (props.flowColumn && "column") || "row"};
+  flex-flow: ${(props) => (props.flowColumn && "column") || "row"};
 `;
